7d40bfa5f15076d8f85ca02c709b974a130e1574
udhayaprakashgithub <146686717+udhayaprakashgithub@users.noreply.github.com>
2026-03-02 21:31:47 +0530

    Add Django manage.py entrypoint script

---
+++ document_processor/services/__init__.py
@@ -0,0 +1,5 @@
+from .document_builder import DocumentBuilder
+from .extraction_engine import ExtractionEngine
+from .textract_service import TextractService
+
+__all__ = ["DocumentBuilder", "ExtractionEngine", "TextractService"]
